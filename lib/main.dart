3dc7b84a3e27931ed4567d6001d6130151e776ca
Rana Asim <asimaltaf037@gmail.com>
2024-06-22 18:23:13 +0500

    notifiction implementation

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 
 @pragma('vm:entry-point')
